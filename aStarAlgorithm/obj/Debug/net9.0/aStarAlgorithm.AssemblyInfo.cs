e0695ae76d461d5f314d728b0d50191bb125762f
Duygu_Akkuş <118392006+DuyguAkkus@users.noreply.github.com>
2025-05-14 01:06:44 +0300

    readme update

--- aStarAlgorithm/obj/Debug/net9.0/aStarAlgorithm.AssemblyInfo.cs
+++ aStarAlgorithm/obj/Debug/net9.0/aStarAlgorithm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aStarAlgorithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07df340bed9247cc5094dba85cd007dfdcde9762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71757aa994b7d7bd5729dae4d4e97fe61d865ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("aStarAlgorithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aStarAlgorithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
